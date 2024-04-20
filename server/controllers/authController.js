f4b0ed38b82d7bff6b0071ff64db9ec96f6629be
Evgeniy Petrov <evgeniy161102@gmail.com>
2024-04-20 09:33:53 +0300

    Добавлена регистрация, и запрос на всех пользователей БД

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,20 @@
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const pool = require("../db");
+const { validationResult } = require("express-validator");
 
-// const jwtSecret = "secret";
+const jwtSecret = "secret-cloud";
 
+// Регистрация
 module.exports.register = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(400)
+      .json({ message: "Ошибка при регистрации", errors: errors.array() });
+  }
+
   const { fullName, email, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(fullName, email, hashedPassword);
@@ -21,12 +31,16 @@ module.exports.register = async (req, res) => {
         .json({ message: "Пользователь с таким email уже существует." });
     }
 
-    const result = await pool.query(
+    const [result] = await pool.query(
       "INSERT INTO `users`( `fullName`, `email`, `password`) VALUES (?,?,?)",
       [fullName, email, hashedPassword]
     );
 
-    console.log("Новый пользователь успешно зарегистрирован => ", result);
+    console.log(
+      "Новый пользователь успешно зарегистрирован => ",
+      fullName,
+      email
+    );
 
     return res.status(201).json({
       message: "Пользователь успешно зарегистрирован.",
@@ -38,4 +52,44 @@ module.exports.register = async (req, res) => {
     console.error(error);
     return res.status(500).json({ message: "Не удалось зарегистрироваться." });
   }
+};
+
+module.exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const [user] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (user.length === 0) {
+      return res.status(404).json({ message: "Пользователь не найден." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user[0].password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Неверный пароль." });
+    }
+
+    const token = jwt.sign({ id: user.id }, jwtSecret, { expiresIn: "1h" });
+
+    return res.json({
+      message: "Пользователь успешно авторизован.",
+      user,
+      token,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Не удалось авторизоваться.", error: err.message });
+  }
+};
+
+// Пользователи
+module.exports.getAllUsers = async (req, res) => {
+  const [result] = await pool.query("SELECT * FROM users");
+  return res.json({
+    users: result,
+  });
 };
--- server/routes/authRoutes.routes.js
+++ server/routes/authRoutes.routes.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const { check } = require("express-validator");
 
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  [
+    check("email", "Некорректный email").isEmail(),
+
+    check("password", "Минимальная длина пароля 6 символов").isLength({
+      min: 6,
+    }),
+  ],
+  authController.register
+);
+router.post("/login", authController.login);
+router.get("/users", authController.getAllUsers);
 
 module.exports = router;
